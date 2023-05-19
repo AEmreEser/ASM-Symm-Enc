0f47c4cdf85379a5b6210619ab264b8da4490267
Emre Eser <emre.eser@sabanciuniv.edu>
2023-05-19 16:48:30 +0300

    store LUT words in dynamic memory function added

--- phase1_template.asm
+++ phase1_template.asm
@@ -25,31 +25,64 @@ li   $a2, 4096     # hardcoded buffer length
 syscall            # read from file
 
 move $s0, $v0	   # the number of characters read from the file
-# la   $s1, buffer   # address of buffer that keeps the characters
+la   $s1, buffer   # address of buffer that keeps the characters
 addi $sp, $sp, -16
 sw $s0, 0($sp) # push s regs
 sw $s1, 4($sp)
 sw $s2, 8($sp)
 sw $s3, 12($sp)
 
+jal table_allocate
+
+li $t0, 256 # t0: number of iterations per LUT
+la $t1, T0 # t1: address of T0 (updated after every iter. of get num val
+
+loop: #fetch numeric val, save it in a single lut Tn
 jal get_num_val # function call
+
+# v0 contains word read from lut
+# s1 contains incremented address of the buffer --> points to the end of the LUT word
+
+sw $v0, 0($t1)
+addi $t1, $t1, 4 # next word in lut
+addi $s1, $s1, 2 # skip ", " between lut vals
+addi $t0, $t0, -1 # decrement
+bnez $t0, loop
+
+# end of loop
 
 lw $s0, 0($sp) # restore s regs
 lw $s1, 4($sp)
 lw $s2, 8($sp)
 lw $s3, 12($sp)
 addi $sp, $sp, 16
+
 j Exit
 
-# 1024 LUT values -- each 4 bytes: total 4096 bytes
+# 1024 LUT values -- each LUT word 4 bytes: total 4096 bytes
 # each LUT will contain 256 entries
 
-# S1: ADDRESS OF BUFFER (set inside get_num_val, passed to func in a1)
+#### FUNCTION Allocate Space
+table_allocate:
+addi $sp, $sp, -4
+sw $t0, 0($sp)
+li $v0, 9 # syscall for dy. mem. alloc
+li $a0, 1024 # 1024 bytes = 256 words
+syscall
+la $t0, T0
+sw $v0, 0($t0)
+lw $t0, 0($sp)
+addi $sp, $sp, 4
+jr $ra
 
+
+
+# S1: ADDRESS OF BUFFER (set inside get_num_val, passed to func in a1)
 
 #### FUNCTION ####
 #### gets the numeric value of table entry pointed by $a1
 # a1 must point to the 0 character in 0x...
+# increments address of buffer: kept in s1
 get_num_val:
 addi $sp, $sp, -12 # push t regs
 sw $t0, 0($sp)
@@ -58,7 +91,7 @@ sw $t2, 8($sp)
 
 li $t1, 0
 li $s3, 0
-move $s1, $a1
+# move $s1, $a1
 addi $s1, $s1, 2 # initial 0x # every 8 iterations: += 4
 func_main:
 lbu $a2, 0($s1) # load from buffer location
